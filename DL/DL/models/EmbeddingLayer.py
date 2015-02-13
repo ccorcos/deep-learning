14a8400df8b049010c7c417afcfd7350295e046e
Chet Corcos <ccorcos@gmail.com>
2015-02-13 13:41:31 -0800

    embedding layer

---
+++ DL/DL/models/EmbeddingLayer.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# coding: utf-8
+
+# import theano
+# import theano.tensor as T
+# import numpy
+# from utils import *
+from HiddenLayer import HiddenLayer
+
+class EmbeddingLayer(HiddenLayer):
+    """Embedding Class
+
+    This class is basically just a linear hidden layer. There will likely be some additional 
+    functionality in this class for visualization layer
+    """
+
+    def __init__(self, rng, input, n_in, n_out, dropout_rate=0, params=None):
+        """ Initialize the parameters of the embedding layer
+
+        input: theano.tensor, matrix of size (n_examples, n_in)
+        n_in: int, number of input units
+        n_out: int, number of output units
+
+        """
+        HiddenLayer.__init__(self, rng, input, n_in, n_out, dropout_rate=dropout_rate, params=params, activation='linear')
